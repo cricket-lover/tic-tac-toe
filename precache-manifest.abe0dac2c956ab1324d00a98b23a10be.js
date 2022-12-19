6dbf89be08c285ecb55240d00008380b431f035b
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2022-12-19 23:01:18 +0530

    Updates

---
+++ precache-manifest.abe0dac2c956ab1324d00a98b23a10be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d9e2e9666eed0ec5604e9e2401905d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996e7fd8615b6a709932",
+    "url": "/static/css/main.1b7cf624.chunk.css"
+  },
+  {
+    "revision": "6690148251a57cd95cfd",
+    "url": "/static/js/2.60b25a51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60b25a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996e7fd8615b6a709932",
+    "url": "/static/js/main.c5bb0b34.chunk.js"
+  },
+  {
+    "revision": "6a52c016a8220507e85f",
+    "url": "/static/js/runtime-main.f1e5c95c.js"
+  }
+]);
